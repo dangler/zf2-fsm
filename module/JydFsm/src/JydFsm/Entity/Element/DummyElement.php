30090629a812444b8683212f4ecdd7ebbb350fd6
jdangler <jdangler@gmail.com>
2014-06-23 21:56:07 -0400

    Missed ORM annotation

--- module/JydFsm/src/JydFsm/Entity/Element/DummyElement.php
+++ module/JydFsm/src/JydFsm/Entity/Element/DummyElement.php
@@ -2,6 +2,15 @@
 
 namespace JydFsm\Entity\Element;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class DummyElement
+ *
+ * @package JydFsm\Entity\Element
+ *
+ * @ORM\Entity
+ */
 class DummyElement extends Element
 {
 }
